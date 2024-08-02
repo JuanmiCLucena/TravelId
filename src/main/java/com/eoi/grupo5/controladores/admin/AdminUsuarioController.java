41d7a3a39c9ce62b39e37a32d05b83fa838d7de8
Miguel Ángel Escudero <miguelangelescuderojimenez@gmail.com>
2024-08-02 13:36:23 +0200

    Creacion UsuarioRegistro, modificación de AdminUsuarioController

--- src/main/java/com/eoi/grupo5/controladores/admin/AdminUsuarioController.java
+++ src/main/java/com/eoi/grupo5/controladores/admin/AdminUsuarioController.java
@@ -51,7 +51,7 @@ public class AdminUsuarioController {
 
     @GetMapping("/crear")
     public String mostrarPaginaCrear(Model modelo) {
-        Usuario usuario = new Usuario();
+        UsuarioRegistro usuario = new UsuarioRegistro();
         modelo.addAttribute("usuario", usuario);
         modelo.addAttribute("detalles", servicioDetallesUsuario.buscarEntidades());
         return "admin/usuarios/adminNuevoUsuario";
@@ -59,14 +59,21 @@ public class AdminUsuarioController {
 
     @PostMapping("/crear")
     public String crear(
-            @ModelAttribute("usuario") Usuario usuario,
-            @RequestParam("detalles.id") Integer detallesUsuarioId
+            @ModelAttribute("usuario") UsuarioRegistro usuarioRegistro
     ) {
 
         try {
 
-            DetallesUsuario detallesUsuario = servicioDetallesUsuario.encuentraPorId(detallesUsuarioId)
-                    .orElseThrow(() -> new IllegalArgumentException("Detalles de Usuario no encontrados"));
+            Usuario usuario = new Usuario();
+            usuario.setPassword(usuarioRegistro.getPassword());
+            usuario.setNombreUsuario(usuarioRegistro.getNombreUsuario());
+
+            DetallesUsuario detallesUsuario = new DetallesUsuario();
+            detallesUsuario.setEmail(usuarioRegistro.getEmail());
+
+            servicioUsuario.guardar(usuario);
+            servicioDetallesUsuario.guardar(detallesUsuario);
+
             usuario.setDetalles(detallesUsuario);
 
             servicioUsuario.guardar(usuario);
